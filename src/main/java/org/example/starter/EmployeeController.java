2087689a5406c45615793a96d69b7cfdb73d9467
AKO352 <arkodeep.koley@maersk.com>
2024-06-11 09:18:14 +0530

    Added some more code to it

--- src/main/java/org/example/starter/EmployeeController.java
+++ src/main/java/org/example/starter/EmployeeController.java
@@ -1,13 +1,28 @@
 package org.example.starter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+
 
 @RestController
 public class EmployeeController {
     HashMap<Integer, Employee> employeeMap=new HashMap<>();
 
+
+   private static Logger log=LoggerFactory.getLogger(EmployeeController.class);
+
+   @GetMapping("/logTest")
+   public String logTest(){
+       log.error("This is an error log");
+       log.warn("This is an error log");
+       log.info("This is an error log");
+       log.debug("This is an error log");
+       log.trace("This is an error log");
+       return "Testing";
+   }
 
     @GetMapping("/search/{name}")
     public String sayHelloWorld(@PathVariable("name") String name){
